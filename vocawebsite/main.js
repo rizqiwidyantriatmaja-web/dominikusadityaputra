6df61f884a66a4fa7422fd9bf6fc56fce31ae76f
rizqiwidyantriatmaja-web <rizqiwidyantriatmaja@gmail.com>
2026-05-26 04:29:49 +0700

    Update main.js

--- vocawebsite/main.js
+++ vocawebsite/main.js
@@ -1,1 +1,12 @@
+const YTPlayerOverlay = document.querySelector(".youtube-player-overlay");
+const YTLinks = document.querySelectorAll(".voca");
 
+YTLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    YTPlayerOverlay.classList.add("active");
+  });
+});
+
+YTPlayerOverlay.addEventListener("click", () => {
+    YTPlayerOverlay.classList.remove("active");
+  });
